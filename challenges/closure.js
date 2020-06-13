fc0e9a7605170b0cbaf694dda85de1fb4a1af270
Chris Engel <Chris.Engel@yahoo.com>
2020-06-12 23:14:17 -0400

    complete

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,29 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: it is due to closure and being able to reach one level above the nested function's scope to gain access to that variable
+  
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const summation = num => {
+  let counter = 0;
+  for(let i = 1; i <= num; i++){
+    counter += i
+  };
+  return counter
+};
+// Recusive solutions, for funsies
+const summation2 = num => {
+  if(num <= 0){
+    return 0
+  } else {
+    return summation2(num - 1);
+  }
+};
+
+console.log("Closure 2", summation(4))
+console.log("Closure 2 Recursion", summation(4))
